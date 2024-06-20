06682ea709557474a604f25301f59b5581705032
anandgin2022@gmail.com <anandgin2022@gmail.com>
2024-06-20 09:01:24 +0530

    model

--- backend/custom_auth/models.py
+++ backend/custom_auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def manager(self,email,phone_number,password=None):
+        if not email:
+            raise ValueError('the email must be set')
+        if not phone_number:
+            raise ValueError('the phone number must be set')
+        email=self.normalize_email(email)
+        user = self.model(email=email,phone_number=phone_number)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
